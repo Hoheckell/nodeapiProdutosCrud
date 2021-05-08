b43e4cd8598a69bb45937eb8b9ade93f1149e502
hoheckell <hoheckell.info@gmail.com>
2021-05-08 17:40:16 -0300

    servicos logout

--- models/Servico.js
+++ models/Servico.js
@@ -17,6 +17,10 @@ module.exports = (Sequelize, DataTypes) => {
         descricao: {
             type: DataTypes.STRING,
             allowNull: true
+        },
+        image: {
+            type: DataTypes.STRING,
+            allowNull: true
         }
     }, {
         sql,
